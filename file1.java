98f6253179b196ad783fd68617da3658768c55ab
RoshiniS5566 <roshisanaboina@gmail.com>
2024-07-06 17:04:07 +0000

    file1 data again committing

--- file1.java
+++ file1.java
@@ -1,1 +1,2 @@
 Here i am going to add some data to move from working are to staging area
+After pushing the code into remote repo i am adding data into local repo and i will push to remote repository
